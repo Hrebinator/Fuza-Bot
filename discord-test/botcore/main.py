04327943ed25370d1f6ab68f2b32886a6880593b
Hrebinator <hrebinator@gmail.com>
2018-03-18 00:08:50 +0100

    idk lol

--- discord-test/botcore/main.py
+++ discord-test/botcore/main.py
@@ -63,10 +63,7 @@ def get_credentials():
     if not credentials or credentials.invalid:
         flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
         flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
+        credentials = tools.run_flow(flow, store, flags)
         print('Storing credentials to ' + credential_path)
     return credentials
     
@@ -214,6 +211,18 @@ async def upcoming():
 async def queue(ctx):
     """queues the events for the day and starts the auto queue"""
     await reQueue(ctx.message)
+    
+@bot.event
+async def on_message(message):
+    print(message.author.name)
+    if (message.author.name == "Hrebinator(Xreni)"):
+        bot.add_reaction(message, ":face_palm")
+        print("oi")
+    bot.process_commands(message)
+    
+@bot.event
+async def on_reaction_add(reaction, user):
+    print(reaction.emoji)
 
 #@bot.event()
 #async def on_error():
